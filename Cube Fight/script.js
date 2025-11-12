494cb97d691c33b2757daa7c631da6081f6cf9b7
Matheus-1313 <cellfonenovo.9@gmail.com>
2025-11-12 20:24:10 -0300

    Add files via upload

---
+++ Cube Fight/script.js
@@ -0,0 +1,12 @@
+let chapeusEl = document.querySelectorAll(".chapeu");
+let carasEl = document.querySelectorAll(".cara");
+let roupasEl = document.querySelectorAll(".roupa");
+
+let posicaoAtualDoChapeuPlayer1 = 0;
+let posicaoAtualDaCaraPlayer1 = 0;
+let posicaoAtualDaRoupaPlayer1 = 0;
+
+let posicaoAtualDoChapeuPlayer2 = 0;
+let posicaoAtualDaCaraPlayer2 = 0;
+let posicaoAtualDaRoupaPlayer2 = 0;
+
