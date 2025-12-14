ef8b1a11a7335e6f1d0ba5adb562663c272b5c8c
Rafael Pierazolle <rfpierr@gmail.com>
2025-12-13 22:22:18 -0300

    Add files via upload

--- script_opcionais.js
+++ script_opcionais.js
@@ -35,3 +35,35 @@ mostrar.reveal(".efeito-opcional-3", {
     delay: 500,
     distance: "90px",
 })
+
+mostrar.reveal(".efeito-opcional-4", {
+    duration: 1000,
+    delay: 750,
+    distance: "90px",
+})
+
+let botaoMenuPrincipalVoltarEl = document.querySelector("#botao-menu-principal-voltar");
+const somVoltarEl = new Audio("efeitos_sonoros/somVoltar.ogg"); somVoltarEl.preload = "auto";
+
+let botaoJaClicado = 0;
+let delaySom;
+
+function voltarParaOMenuPrincipal() {
+    if(!botaoJaClicado){
+        delay = 0;
+        botaoJaClicado = 1;
+    }
+    else 
+        delaySom = 500;
+    
+    setTimeout(() => {
+        somVoltarEl.currentTime = 0;
+        somVoltarEl.play();    
+    }, delaySom)
+
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 500);
+}
+
+botaoMenuPrincipalVoltarEl.addEventListener('click', voltarParaOMenuPrincipal);
